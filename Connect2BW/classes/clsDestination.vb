764a142bfdccc9e6f338f17fc06e78d0c7c3598d
Tobias Meyer <reyemsaibot@gmail.com>
2018-01-14 15:30:38 +0100

    User & Destination Class

---
+++ Connect2BW/classes/clsDestination.vb
@@ -0,0 +1,23 @@
+﻿Public Class clsDestination
+
+#Region "Variables"
+
+    Private _applicationServer As String
+    Private _systemNumber As Short
+    Private _systemId As String
+    Private _routerString As String
+
+#End Region
+
+#Region "Constructors"
+
+    Friend Sub New()
+        _applicationServer = String.Empty
+        _systemNumber = 0
+        _systemId = String.Empty
+        _routerString = String.Empty
+    End Sub
+
+#End Region
+
+End Class
---
+++ Connect2BW/classes/clsUser.vb
@@ -0,0 +1,27 @@
+﻿Public Class clsUser
+
+#Region "Variables"
+
+    Private _userKey As String
+    Private _systemIdentifier As String
+    Private _client As String
+    Private _userName As String
+    Private _password As String
+    Private _language As String
+
+#End Region
+
+#Region "Constructors"
+
+    Friend Sub New()
+        _systemIdentifier = String.Empty
+        _client = String.Empty
+        _userName = String.Empty
+        _password = String.Empty
+        _language = String.Empty
+        _userKey = _userName & "@" & _systemIdentifier
+    End Sub
+
+#End Region
+
+End Class
